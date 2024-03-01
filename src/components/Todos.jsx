230e6d2d57c6f3c7d29212242bd134b2dd0c978d
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-03-02 05:07:47 +0530

    todo_march_revision

---
+++ src/components/Todos.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export function Todos(){
+
+    const[todo, setTodo] = useState([{
+        title : "Learn react",
+        description : "from 7 to 9",
+        completed : false
+    },
+    {
+        title : "Learn express",
+        description : "from 9 to 11",
+        completed : false   
+    },
+    {
+        title : "Learn rust",
+        description : "from 11 to 1",
+        completed : false
+    }
+
+])
+
+
+return <div>
+    {todo.map((todo) => {
+        return <div>
+            <h1>{todo.title}</h1>
+            <h2>{todo.description}</h2>
+            <button>{completed == true ? "Completed" : "Mark as done"}</button>
+        </div>
+    })}
+</div>
+
+
+}
