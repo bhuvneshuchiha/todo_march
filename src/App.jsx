7804336fe0857043693ef14a3025de5edf3b4a51
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-03-02 05:16:09 +0530

    todo_march_more_changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { CreateTodo } from './components/CreateTodo'
+import { Todos } from './components/Todos'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <CreateTodo></CreateTodo>
+     <Todos></Todos>
     </>
   )
 }
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -26,7 +26,7 @@ return <div>
         return <div>
             <h1>{todo.title}</h1>
             <h2>{todo.description}</h2>
-            <button>{completed == true ? "Completed" : "Mark as done"}</button>
+            <button>{todo.completed == true ? "Completed" : "Mark as done"}</button>
         </div>
     })}
 </div>
